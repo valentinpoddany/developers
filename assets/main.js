733ca9124e0ac524c9813c07d1bfd9bfb912d02b
Rémi Delhaye <contact@rdlh.io>
2016-06-15 16:50:15 +0200

    Clean code

--- assets/main.js
+++ assets/main.js
@@ -2,3 +2,10 @@ $('a.has-child').click(function() {
   $(this).toggleClass('active')
   $(this).toggleClass('open')
 })
+
+var scriptTag = document.createElement('script');
+scriptTag.src = 'https://cdn.slaask.com/chat.js';
+document.getElementsByTagName('head')[0].appendChild(scriptTag);
+scriptTag.onload = function() {
+  _slaask.init('6e32de720b47555bdc81696b3fa955b1');
+}
