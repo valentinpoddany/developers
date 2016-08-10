a1fcb771bb6404373381889b942818295ff71731
Rémi Delhaye <contact@rdlh.io>
2016-08-10 16:45:46 +0200

    Update getting more informations

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,37 @@
-$('a.has-child').click(function() {
-  $(this).toggleClass('active')
-  $(this).toggleClass('open')
+$(function() {
+  $('a.has-child').click(function() {
+    $(this).toggleClass('active')
+    $(this).toggleClass('open')
+  })
+
+  $('.btn').click(function() {
+    $('.code-window .btn').removeClass('active');
+    $(this).addClass('active');
+
+    $('.code-window pre').addClass('hidden');
+    $('#' + this.id.split('-')[1]).removeClass('hidden');
+  })
+
+  $('.hover-swag').hover(function() {
+    $('.code-window .token.property').addClass('big-property')
+  }, function() {
+    $('.code-window .token.property').removeClass('big-property')
+  })
+
+  (function() {
+    var slk = document.createElement('script');
+    slk.src = 'https://cdn.slaask.com/chat.js';
+    slk.type = 'text/javascript';
+    slk.async = 'true';
+    slk.onload = slk.onreadystatechange = function() {
+      var rs = this.readyState;
+      if (rs && rs != 'complete' && rs != 'loaded') return;
+      try {
+        _slaask.init('ef8f4787e310a053be820c434e35ea2c');
+      } catch (e) {}
+    };
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(slk, s);
+  })();
 })
 
-var scriptTag = document.createElement('script');
-scriptTag.src = 'https://cdn.slaask.com/chat.js';
-document.getElementsByTagName('head')[0].appendChild(scriptTag);
-scriptTag.onload = function() {
-  _slaask.init('ef8f4787e310a053be820c434e35ea2c');
-}
